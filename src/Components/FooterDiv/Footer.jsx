4cc12245ef8cbb1baf918411a11be1d3b0357979
Jemeel Sanni <jemeelsanni@gmail.com>
2023-07-04 16:43:55 -0400

    Done

---
+++ src/Components/FooterDiv/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Imported Icons ========>
+import { AiFillInstagram } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer p-[5rem] mb-4 bg-blueColor rounded-[10px] m-auto items-start justify-center">
+      <p class="text-center text-white">Created by Jemeel Sanni</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="navBar flex justify-between items-center p-[3rem]">
+      <div className="logoDiv">
+        <h1 className="logo text-[25px] text-blueColor">
+          <strong>Jobbers</strong>Link
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
